9ce811bbdfeefd03a7c96804dd259d1ad518e6c1
zramsay <zach@bluecollarcoding.ca>
2019-04-04 13:00:22 -0400

    get basic :snake: working

---
+++ cmd/stake-dist/commands/stake-dist.go
@@ -0,0 +1,79 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+
+	gaia "github.com/cosmos/cosmos-sdk/cmd/gaia/app"
+	"github.com/interchainio/delegation/pkg"
+	tmclient "github.com/tendermint/tendermint/rpc/client"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	cdc = gaia.MakeCodec()
+
+	// expects a locally running node
+	node = tmclient.NewHTTP("localhost:26657", "/websocket")
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "stake-dist",
+	Short: "Display stake distribution at latest block height",
+	Long: `Display how many validators control 1/3 and 2/3 of
+	the total stake, at the latest block height.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("woot woot working")
+	},
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	var toAdd float64
+	if len(args) == 1 {
+		toAddInt, err := strconv.Atoi(args[0])
+		if err != nil {
+			panic(err)
+		}
+		toAdd += float64(toAddInt)
+	}
+
+	// get list of validators and sort descending by power
+	validators := pkg.GetValidators(cdc, node)
+	sort.Slice(validators, func(i, j int) bool {
+		return validators[i].Tokens.GT(validators[j].Tokens)
+	})
+
+	var totalStaked float64 = 0
+	for _, v := range validators {
+		staked := pkg.UatomIntToAtomFloat(v.Tokens)
+		totalStaked += staked
+	}
+	totalStaked += toAdd
+
+	var accum float64 = 0
+	oneThird := 0
+	twoThirds := 0
+	for i, v := range validators {
+		staked := pkg.UatomIntToAtomFloat(v.Tokens)
+		accum += staked
+		if twoThirds == 0 && accum > 0.666666666666*totalStaked {
+			twoThirds = i + 1
+		}
+		if oneThird == 0 && accum > 0.333333333333*totalStaked {
+			oneThird = i + 1
+		}
+	}
+	fmt.Printf("At block height %d\n", pkg.GetLatestHeight(node))
+	fmt.Printf("%d validators control 2/3 of the stake\n", twoThirds)
+	fmt.Printf("%d validators control 1/3 of the stake\n", oneThird)
+}
