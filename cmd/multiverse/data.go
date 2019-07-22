23c7e6ed91ce1e02b74d615fe2ce6077b4ff4210
Ethan Buchman <ethan@coinculture.info>
2019-07-22 14:50:59 -0400

    data.go file

---
+++ cmd/multiverse/data.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+const UATOM_PER_ATOM = 1000000
+
+// csvData contained in the csv file
+type csvData struct {
+	Amount  float64 // atoms, up to 6 decimals
+	Address string  // cosmos1 acc address
+}
+
+func (c csvData) ToMsgSend(from sdk.AccAddress) (msg bank.MsgSend, err error) {
+	amount := int64(c.Amount * UATOM_PER_ATOM)
+	addr, err := sdk.AccAddressFromBech32(c.Address)
+	if err != nil {
+		return msg, err
+	}
+	return bank.MsgSend{
+		FromAddress: from,
+		ToAddress:   addr,
+		Amount:      sdk.Coins{sdk.NewInt64Coin("uatom", amount)},
+	}, nil
+}
+
+func (c csvData) ToMsgDelegate(from sdk.AccAddress) (msg stakingtypes.MsgDelegate, err error) {
+	amount := int64(c.Amount * UATOM_PER_ATOM)
+	addr, err := sdk.ValAddressFromBech32(c.Address)
+	if err != nil {
+		return msg, err
+	}
+	return stakingtypes.MsgDelegate{
+		DelegatorAddress: from,
+		ValidatorAddress: addr,
+		Value:            sdk.NewInt64Coin("uatom", amount),
+	}, nil
+}
+
+func readTxData(fileName string) ([]csvData, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var data []csvData
+	for i, record := range records {
+		addr := record[0]
+		amount, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			return nil, fmt.Errorf("Error on line %d: %v", i, err)
+		}
+		data = append(data, csvData{amount, addr})
+	}
+	return data, nil
+}
